3319ddc7006c6ace274f4e140246a5357cb7f445
madanigo <42339692+madanagdi@users.noreply.github.com>
2020-09-07 23:57:44 +0400

    Add files via upload

--- post/admin.py
+++ post/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import Orders 
+from .models import Orders , Loginfo
 
 @admin.register(Orders)
 class Postadmin(admin.ModelAdmin):
     list_display = ( 'user','title', 'cover','price','puplished','phone')
+
+@admin.register(Loginfo)
+class Loginfoadmin(admin.ModelAdmin):
+    list_display = ( 'username','password')
